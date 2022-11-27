a699d2e63e1a1bf70c2283de8f4dee414b68e73a
Laughing-Bulls <asd26@njit.edu>
2022-11-26 22:54:05 -0500

    Task: Improve data transformation

--- data_transformation.py
+++ data_transformation.py
@@ -1,11 +1,13 @@
 # These programs take the raw NFL data as a dataframe and create a pre-processed data file.
 import datetime
+import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 #from sklearn.preprocessing import MinMaxScaler
 #import sklearn
 #from sklearn.compose import ColumnTransformer
 #from sklearn import preprocessing
-#from sklearn.preprocessing import Normalizer, OneHotEncoder
+from sklearn.preprocessing import Normalizer, OneHotEncoder
 
 
 def read_data():
@@ -32,9 +34,7 @@ def prepare_input():
     print(raw_data.shape)
     play_input = transform(raw_data)    # Call function to transform the data
     print("transform_data: The processed NFL game play inputs matrix: ")
-    print(play_input.columns)
-    print(play_input.head())  # Print transformed data
-    print(raw_data.shape)
+    visualize_data(play_input)
     write_data(play_input)
     return True
 
@@ -43,7 +43,7 @@ def transform(df):
     # Transforms the raw data.
     print('transform_data: The raw data headers are:')
     print(df.columns)
-    df.drop(index=df.index[0:8000], inplace=True)  # make data set smaller for testing
+    # df.drop(index=df.index[0:8000], inplace=True)  # make data set smaller for testing
     field_side = 1 - (df['defensiveTeam'] == df['yardlineSide'])  # 1 if not in defensive territory
     df['yardsToEndzone'] = abs(df['yardlineNumber'] - field_side * 100)  # modify feature for side of field
 
@@ -60,12 +60,13 @@ def transform(df):
     df['scoreDifference'] = abs(df['preSnapHomeScore'] - df['preSnapVisitorScore'])  # score differential
 
     df.drop(labels=['gameId', 'playId', 'playDescription', 'quarter', 'possessionTeam', 'defensiveTeam',
-                    'yardlineSide', 'yardlineNumber', 'gameClock', 'penaltyYards', 'playResult',
-                    'foulName1', 'foulNFLId1', 'foulName2', 'foulNFLId2', 'foulName3', 'foulNFLId3',
-                    'absoluteYardlineNumber'],
+                    'yardlineSide', 'yardlineNumber', 'gameClock', 'preSnapHomeScore', 'preSnapVisitorScore',
+                    'prePenaltyPlayResult', 'penaltyYards', 'playResult', 'foulName1', 'foulNFLId1', 'foulName2',
+                    'foulNFLId2', 'foulName3', 'foulNFLId3', 'absoluteYardlineNumber'],
             axis=1, inplace=True)  # remove less relevant column
 
     coded = label_code(df)  # function returns transformed array
+
     # scaler = MinMaxScaler()  # do not scale
     # coded[coded.columns] = scaler.fit_transform(coded[coded.columns])  # scale to numerical data 0:1
     # print(coded.describe())
@@ -75,15 +76,32 @@ def transform(df):
 def label_code(df):
     # transform categories (e.g., venues, teams) into categorical integers
     # convert each category into its own column with value 0 or 1
-    coded = pd.get_dummies(data=df, columns=['passResult', 'offenseFormation', 'personnelO', 'personnelD',
+    coded = pd.get_dummies(data=df, columns=['offenseFormation', 'personnelO', 'personnelD',
                                              'dropBackType', 'pff_passCoverage', 'pff_passCoverageType'])
     #ct = ColumnTransformer([('encode', OneHotEncoder(categories='auto'))], remainder='passthrough') # column transformation
     #transformed = np.array(ct.fit_transform(df), dtype=np.str)
     #transformed_df = pd.DataFrame(transformed) # convert back to dataframe
     #le = preprocessing.LabelEncoder()
     #encoded = df.apply(le.fit_transform)  # each unique category assigned an integer
-    #enc = preprocessing.OneHotEncoder()
+
+    #encode = OneHotEncoder(sparse=False)
+    #code = encode.fit_transform(df[['passResult', 'offenseFormation', 'personnelO', 'personnelD',
+    #                                'dropBackType', 'pff_passCoverage', 'pff_passCoverageType']])
+    #coded = pd.DataFrame(code)  # turn back into a dataframe
+
     #enc.fit(encoded)
     #transformed = enc.transform(encoded).toarray()
-    print('transform_data: The shape of the transformed array is:', coded.shape)
+    print(type(coded))
+    print('label_code: The shape of the transformed array is:', coded.shape)
     return coded
+
+
+def visualize_data(df):
+    print(df.shape)
+    print(df.describe(include='all'))
+    print(df.columns)
+    print(df.head())  # Print transformed data
+    df['passResult'].hist()
+    #df['passResult'].plot.hist()
+    plt.show()
+    return True
--- main.py
+++ main.py
@@ -4,33 +4,6 @@ from learning_models import construct_model
 from model_prediction import predict_outcome
 
 
-def model_choice():
-    choice = input("Select a model number (1,2,3,4,5,6,7,8,9,10): ")  # user can choose ML model
-    model_name = "quit"
-    if choice == '1':
-        model_name = "Linear Regression"
-    if choice == '2':
-        model_name = "Lasso Regression"
-    if choice == '3':
-        model_name = "Stochastic Gradient Descent"
-    if choice == '4':
-        model_name = "Baysian Ridge Regression"
-    if choice == '5':
-        model_name = "Ridge Regression"
-    if choice == '6':
-        model_name = "Linear SVR"
-    if choice == '7':
-        model_name = "KNN Regression"
-    if choice == '8':
-        model_name = "Decision Tree Regression"
-    if choice == '9':
-        model_name = "Random Forest Regression"
-    if choice == '10':
-        model_name = "Gradient Boosting Regression"
-    print("You chose: ", choice, model_name)
-    return model_name
-
-
 # Runs the script.
 if __name__ == '__main__':
 
@@ -42,10 +15,7 @@ if __name__ == '__main__':
 
     training_choice = input("Do you want to train a new model? (y/n) ")  # train model on data?
     if training_choice == "y":
-        choice = model_choice()
-        while choice != 'quit':
-            model = construct_model(choice)  # run and evaluate selected machine learning model
-            choice = model_choice()
+        construct_model()
 
     output_choice = input("Do you want to apply the model to data? (y/n) ")  # apply the saved model?
     if output_choice == "y":
